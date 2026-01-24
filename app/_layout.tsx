10ab39ab45434e0a1a6b15bcd118dcdbd803c08a
BoomCheeseBurge <brandonjohnlyc@gmail.com>
2026-01-24 09:31:42 +0700

    Setup Nativewind, Icons, and Fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,28 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+
+    const [fontsLoaded, error] = useFonts({
+        "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+        "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+        "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+        "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+        "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    });
+
+    // Load the fonts as soon as the app is up and running 
+    useEffect(() => {
+      
+        if(error) throw error;
+    
+        // Hides splash screen once fonts are loaded in the app
+        if(fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error]);
+
+    if (!fontsLoaded && !error) return null;
+    
+    return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
-
+ 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-4xl text-center text-primary font-quicksand-bold">
+                Welcome to my Food Delivery App!
+            </Text>
+        </View>
+    );
 }
