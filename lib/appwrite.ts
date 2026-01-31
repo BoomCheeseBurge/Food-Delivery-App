a5d44cf6dfc8affee3fd20d546d3cdb677c55381
BoomCheeseBurge <brandonjohnlyc@gmail.com>
2026-01-31 22:30:28 +0700

    Setup Appwrite Config

---
+++ lib/appwrite.ts
@@ -0,0 +1,6 @@
+export const config = {
+
+    platform: 'com.expo.fooddelivery',
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
